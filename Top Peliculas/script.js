c99b186a5d241670fec0bc066bd48ced0461620b
Octavio Guerrero <97319614+octav20@users.noreply.github.com>
2022-06-22 02:28:12 -0600

    Add files via upload

---
+++ Top Peliculas/script.js
@@ -0,0 +1,6 @@
+const toggleMenu = document.getElementById("toggle-menu");
+const mainMenu = document.getElementById("main-menu");
+
+toggleMenu.addEventListener("click",()=>{
+    mainMenu.classList.toggle("main-menu--show")
+});
